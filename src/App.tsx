9299597434e80446d1e24170f6bd63ccc2e23c77
utgwkk <utagawakiki@gmail.com>
2021-07-10 00:53:35 +0900

    calculate component size with animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 import { Child } from "./Child";
 import styles from "./App.module.css";
+import { useState } from "react";
 
 export const App: React.VFC = () => {
+  const [done, setDone] = useState(false);
+
   return (
     <div className="App">
       <h1>Hello, world!</h1>
-      <div className={styles.container}>
-        <Child />
-      </div>
+      <motion.div
+        initial={{ scale: 0 }}
+        animate={{ scale: 1 }}
+        transition={{ duration: 0.3 }}
+        className={styles.container}
+        onAnimationComplete={() => setDone(true)}
+      >
+        <Child animationCompleted={done} />
+      </motion.div>
     </div>
   );
 };
--- src/Child.tsx
+++ src/Child.tsx
@@ -1,8 +1,19 @@
 import React from "react";
+import { useEffect } from "react";
 import useMeasure from "react-use-measure";
 
-export const Child: React.VFC = () => {
-  const [ref, { width }] = useMeasure();
+interface ChildProps {
+  animationCompleted: boolean;
+}
+
+export const Child: React.VFC<ChildProps> = ({ animationCompleted }) => {
+  const [ref, { width }, recalculate] = useMeasure();
+
+  useEffect(() => {
+    if (animationCompleted) {
+      recalculate();
+    }
+  }, [animationCompleted, recalculate]);
 
   return (
     <div ref={ref}>
